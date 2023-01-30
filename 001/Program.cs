e9e08dd8379d419597c9716f7e6afd4b00bca135
aleksandr sosuev <sav_ml@mail.ru>
2023-01-30 16:36:19 +0500

    Lesson One - 1

---
+++ 001/Program.cs
@@ -0,0 +1,8 @@
+﻿// 1. С клавиатуры вводится целое число. Вывести квадрат числа
+checked
+{
+    System.Console.Write("Введите целое число: ");
+    string? s = Console.ReadLine();
+    int a = Convert.ToInt32(s);
+    System.Console.WriteLine("Квадрат введенного числа: " + Math.Pow(a, 2));
+}
