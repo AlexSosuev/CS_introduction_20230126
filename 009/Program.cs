a8af4cd5eff00cb051dad4e101a00868432e9332
aleksandr sosuev <sav_ml@mail.ru>
2023-01-30 17:28:23 +0500

    Lesson One - 9

---
+++ 009/Program.cs
@@ -0,0 +1,9 @@
+﻿// 9. Вывести на экран четные числа от 1 до N
+System.Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= N)
+{
+    System.Console.WriteLine(i);
+    i += 2;
+}
