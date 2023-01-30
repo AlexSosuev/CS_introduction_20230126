2e8e9d26055a553d3785df61e40bc747f9b23b62
aleksandr sosuev <sav_ml@mail.ru>
2023-01-30 19:57:42 +0500

    Lesson One - 6

---
+++ 006/Program.cs
@@ -0,0 +1,4 @@
+﻿// 6. Написать программу вычисления значения функции y = sin(a). (Класс Math)
+System.Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Math.Sin(a));
