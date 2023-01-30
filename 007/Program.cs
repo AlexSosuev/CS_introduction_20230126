efc7d6d9fa28181833ad1206268e87ddcda8e1ab
aleksandr sosuev <sav_ml@mail.ru>
2023-01-30 17:53:56 +0500

    Lesson One - 7

---
+++ 007/Program.cs
@@ -0,0 +1,8 @@
+﻿// 7. Выяснить, является ли число чётным?
+System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+bool odd = N % 2 == 0;
+if (odd == true)
+    System.Console.WriteLine($"Число {N} чётное");
+else
+    System.Console.WriteLine($"Число {N} не чётное");
