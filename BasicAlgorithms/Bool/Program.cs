b69ee4376b127103c32a2dbea93c2995e978e29a
aleksandr sosuev <sav_ml@mail.ru>
2023-01-31 00:10:32 +0500

    Lesson 2

---
+++ BasicAlgorithms/Bool/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine($"{System.Int16.MinValue} {System.Int16.MaxValue}");
+System.Console.WriteLine($"{System.Int32.MinValue} {System.Int32.MaxValue}");
+System.Console.WriteLine($"{System.Int64.MinValue} {System.Int64.MaxValue}");
+System.Console.WriteLine($"{System.Int128.MinValue} {System.Int128.MaxValue}");
+
+bool a = false;
+System.Boolean b = true;
+bool c1 = 2 * 2 == 4;
+bool c2 = 2 * 2 != 5;
+bool c3 = 2 * 2! > 5;
+bool c4 = 2 * 2! < 5;
+bool c5 = 2 * 2! >= 5;
+bool c6 = 2 * 2! <= 5;
