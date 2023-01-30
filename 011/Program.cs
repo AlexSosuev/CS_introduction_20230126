28cbb05119fd8d005f3b05666a7474dfc2044be7
aleksandr sosuev <sav_ml@mail.ru>
2023-01-30 17:44:19 +0500

    Lesson One - 11

---
+++ 011/Program.cs
@@ -0,0 +1,4 @@
+﻿// 11. Дано число больше 9. Ввести на экран вторую цифру числа с конца
+System.Console.Write("Введите число, больше 9: ");
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"{N / 10 % 10}");
