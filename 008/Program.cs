89fb37494129d2acef13c3914fe5c07923c8e536
aleksandr sosuev <sav_ml@mail.ru>
2023-01-30 20:05:23 +0500

    Lesson One - 8

---
+++ 008/Program.cs
@@ -0,0 +1,9 @@
+﻿// 8. Вывести на экран числа от -N до N
+System.Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = -N;
+while (i <= N)
+{
+    System.Console.WriteLine(i);
+    i++;
+}
