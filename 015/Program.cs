e7276cc8d3bdb0121bfdaa0a7503a6984cfa8a99
aleksandr sosuev <sav_ml@mail.ru>
2023-01-30 19:09:19 +0500

    Lesson One - 15

---
+++ 015/Program.cs
@@ -0,0 +1,11 @@
+﻿// 15. С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет (Вывести: NO)
+System.Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 100)
+    System.Console.WriteLine($"Нет третьей цифры в введенном числе");
+else
+{
+    while (N >= 1000)
+        N /= 10;
+    System.Console.WriteLine($"Третья цыфра введенного числа равна {N % 10}");
+}
