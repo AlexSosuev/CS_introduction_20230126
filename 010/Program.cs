38335585bca90ce8a539acb328d022fbfc2e0096
aleksandr sosuev <sav_ml@mail.ru>
2023-01-30 19:13:45 +0500

    Lesson One - 10

---
+++ 010/Program.cs
@@ -0,0 +1,4 @@
+﻿// 10. Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"{N % 10}");
