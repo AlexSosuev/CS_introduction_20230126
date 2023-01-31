5cd01284f4290ccfa1ae34d46e74dbb95659324b
aleksandr sosuev <sav_ml@mail.ru>
2023-01-31 08:34:35 +0500

    CounterAndSummer

---
+++ BasicAlgorithms/CounterAndSummer/Program.cs
@@ -0,0 +1,13 @@
+﻿//Подсчитать количество и сумму цифр
+int N = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+int sum = 0;
+while (N != 0)
+{
+    counter++;
+    int d = N % 10;
+    sum += d;
+    N /= 10;
+}
+System.Console.WriteLine(counter);
+System.Console.WriteLine(sum);
