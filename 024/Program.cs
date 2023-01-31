e6b1b0d2b94b738f7a70caa1c1f1c5f3e2186cb7
aleksandr sosuev <sav_ml@mail.ru>
2023-01-31 15:29:53 +0500

    Lesson Two - 24

---
+++ 024/Program.cs
@@ -0,0 +1,9 @@
+﻿// 24. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+System.Console.Write("Введите координаты точки: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+if (x == 0 || y == 0) System.Console.WriteLine("Не правильно введены координаты");
+else if (x > 0 && y > 0) System.Console.WriteLine("Точка в 1-й четверти плоскости");
+else if (x < 0 && y > 0) System.Console.WriteLine("Точка во 2-й четверти плоскости");
+else if (x < 0 && y < 0) System.Console.WriteLine("Точка в 3-й четверти плоскости");
+else if (x > 0 && y < 0) System.Console.WriteLine("Точка в 4-й четверти плоскости");
