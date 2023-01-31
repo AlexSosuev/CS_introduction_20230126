c4b9100db332902141affd3146915b8e6fafae90
aleksandr sosuev <sav_ml@mail.ru>
2023-01-31 13:01:06 +0500

    Lesson Two - 22

---
+++ 022/Program.cs
@@ -0,0 +1,7 @@
+﻿// 22. По двум заданным числам проверять является ли одно квадратом другого
+System.Console.Write("Введите числа a и b: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b) System.Console.WriteLine("b является квадратом a");
+else if (b * b == a) System.Console.WriteLine("a является квадратом b");
+else System.Console.WriteLine("Введенные числа не являются квадратами друг друга!");
