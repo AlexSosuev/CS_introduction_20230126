b19ed5cc084109a85e7be20b62e7708c51172c67
aleksandr sosuev <sav_ml@mail.ru>
2023-01-31 15:47:13 +0500

    Lesson Two - 26

---
+++ 026/Program.cs
@@ -0,0 +1,9 @@
+﻿// 26. Программа проверяет пятизначное число на палиндромом
+System.Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a1 = n % 10;
+int a2 = n / 10000;
+int b1 = n % 100 / 10;
+int b2 = n / 1000 % 10;
+if (a1 == a2 && b1 == b2) System.Console.WriteLine("Полиндром");
+else System.Console.WriteLine("Не полиндром");
