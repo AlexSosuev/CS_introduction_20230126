6847fa59b54f8d9135485cbccf3a3bf21aecf96d
aleksandr sosuev <sav_ml@mail.ru>
2023-01-31 12:34:22 +0500

    Lesson Two - 21(2.0)

--- 021/Program.cs
+++ 021/Program.cs
@@ -3,6 +3,6 @@ System.Console.Write("Введите номер дня недели: ");
 int day = Convert.ToInt32(Console.ReadLine());
 if (day == 6 || day == 7) System.Console.WriteLine("Выходной");
 else
-    if (day >= 1 && day <= 7) System.Console.WriteLine("Рабочий день");
+    if (day >= 1 && day <= 5) System.Console.WriteLine("Рабочий день");
 else
     System.Console.WriteLine("Это не день недели");
