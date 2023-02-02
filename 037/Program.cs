5e3c2a0ebe265a73fbc038dce13569bde7915f2e
aleksandr sosuev <sav_ml@mail.ru>
2023-02-02 23:58:47 +0500

    Lesson three-037

---
+++ 037/Program.cs
@@ -0,0 +1,14 @@
+﻿// 37. Компютер придумывает число и определяет заканчивается ли на четную цифру куб этого числа.
+bool Test(int n)
+{
+    return n % 2 == 0;
+}
+
+Random random = new Random();
+for (int i = 0; i < 10; i++)
+{
+    int a = random.Next(1, 100);
+    int b = (int)Math.Pow(a, 3);
+    if (Test(a))
+        System.Console.WriteLine($"{a}, {b}");
+}
