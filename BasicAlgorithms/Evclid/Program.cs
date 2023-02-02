fe6bfa56d3a75b1b5a088376f1ce09a5f3395936
aleksandr sosuev <sav_ml@mail.ru>
2023-02-02 23:22:38 +0500

    Evclid

---
+++ BasicAlgorithms/Evclid/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 75, b = 35;
+int a1 = a, b1 = b;
+while (a != b)
+{
+    if (a > b) a -= b;
+    else b -= a;
+}
+System.Console.WriteLine("НОД - " + a);
+System.Console.WriteLine("НОК - " + (a1 * b1) / a);
