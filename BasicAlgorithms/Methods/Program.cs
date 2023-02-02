47172f83fba6bec4bba9eb735df1d4d5f0ce98f9
aleksandr sosuev <sav_ml@mail.ru>
2023-02-02 23:04:59 +0500

    Methods

---
+++ BasicAlgorithms/Methods/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите подпрограмму нахождения расстояния между двумя точками
+double Distance(double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+}
+
+double distance = Distance(0, 0, 2, 2);
+System.Console.WriteLine(distance);
