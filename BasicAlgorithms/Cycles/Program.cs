00a60d94b62bf47a532abf04e33ae2aadde2ad38
aleksandr sosuev <sav_ml@mail.ru>
2023-02-02 22:39:09 +0500

    Cycles_2

--- BasicAlgorithms/Cycles/Program.cs
+++ BasicAlgorithms/Cycles/Program.cs
@@ -1,7 +1,20 @@
 ﻿// Ввести n от 0 до 10 с проверкой ввода
-int n;
-do
+// Цикл ДО
+//int n;
+//do
+//{
+//    n = Convert.ToInt32(Console.ReadLine());
+//}
+//while (n < 0 || n > 10);
+
+//Цикл ДЛЯ (FOR)
+int i = 1;
+while (i <= 10)
 {
-    n = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine(i);
+    i++;
 }
-while (n < 0 || n > 10);
+for (int j = 1; j <= 10; j++)
+{
+    System.Console.WriteLine(j);
+}
