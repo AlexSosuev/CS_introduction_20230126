490c474c561b17eeab49afd6f3967d5bb54d9f7b
aleksandr sosuev <sav_ml@mail.ru>
2023-02-03 15:36:34 +0500

    Lesson three - 036

---
+++ 036/Program.cs
@@ -0,0 +1,14 @@
+﻿// 36. Написать программу вычисления произведения чисел от 1 до N
+int multiOfDigits(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+System.Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Произведение чисел от 1 до n равно " + multiOfDigits(n));
