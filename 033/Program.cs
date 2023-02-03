6d2f600c4992470ff1d606b7f4e7e279942b0194
aleksandr sosuev <sav_ml@mail.ru>
2023-02-03 15:21:40 +0500

    Lesson three - 033

---
+++ 033/Program.cs
@@ -0,0 +1,15 @@
+﻿// 33.Возведите число А в натуральную степень B используя цикл
+int exponentiation(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+System.Console.Write("Введите число A и натуральную степень B: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(exponentiation(A, B));
