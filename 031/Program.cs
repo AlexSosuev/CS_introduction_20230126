329f6aea3ae436d4d7f2fcfc87285ba2da90433f
aleksandr sosuev <sav_ml@mail.ru>
2023-02-03 15:11:51 +0500

    Lesson three - 031

---
+++ 031/Program.cs
@@ -0,0 +1,12 @@
+﻿// 31.Вывести на экран кубы чисел от 1 до N
+int kubic(int n)
+{
+    return (int)Math.Pow(n, 3);
+}
+
+System.Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    System.Console.WriteLine(kubic(i));
+}
