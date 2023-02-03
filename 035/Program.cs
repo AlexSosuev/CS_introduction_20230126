6f926af21d3f1d17820c83c6ba1dfbd1ee75ee83
aleksandr sosuev <sav_ml@mail.ru>
2023-02-03 15:30:31 +0500

    Lesson three - 035

---
+++ 035/Program.cs
@@ -0,0 +1,15 @@
+﻿// 35. Подсчитать сумму цифр в числе. Сделать подпрограмму
+int sumOfDigits(int n)
+{
+    int result = 0;
+    while (n != 0)
+    {
+        result += n % 10;
+        n /= 10;
+    }
+    return result;
+}
+
+System.Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Сумма цифр в числе n равна " + sumOfDigits(n));
