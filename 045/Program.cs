b07aea9c30a51b8a776f36e5cb86ef38e4994af4
Alexander Sosuev <sav_ml@mail.ru>
2023-02-07 09:40:35 +0500

    Lesson four - 045

---
+++ 045/Program.cs
@@ -0,0 +1,16 @@
+﻿// 45. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+int[] a=new int[8];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(100,1000);
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i]} ");
+System.Console.WriteLine();
+
+int chet=0, neChet=0;
+for(int i=0;i<a.Length;i++)
+    if (a[i]%2==0) chet++;
+    else neChet++;
+
+System.Console.WriteLine($"Четных - {chet}, нечетных - {neChet}");
