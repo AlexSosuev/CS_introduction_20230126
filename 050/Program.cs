afccbb10aad01b90031ec43368f0d55d8e6012f0
aleksandr sosuev <sav_ml@mail.ru>
2023-02-10 00:15:32 +0500

    Lesson Five - 050

---
+++ 050/Program.cs
@@ -0,0 +1,5 @@
+﻿// 50. Выяснить являются ли три числа сторонами треугольника
+int a = 4, b = 3, c = 4;
+if (a + b > c && a + c > b && b + c > a)
+    System.Console.WriteLine("Являются");
+else System.Console.WriteLine("Не являются");
