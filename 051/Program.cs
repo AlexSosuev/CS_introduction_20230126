db092dc18168debceccbc0d8b401b75c1e30773b
aleksandr sosuev <sav_ml@mail.ru>
2023-02-09 23:11:56 +0500

    Lesson Five - 051

---
+++ 051/Program.cs
@@ -0,0 +1,21 @@
+﻿// 51. С клавиатуры вводится число N. Затем вводится N чисел.
+namespace GeekBrains
+{
+    namespace Introduction
+    {
+        class Task051
+        {
+            static void Main()
+            {
+                int N = Convert.ToInt32(Console.ReadLine());
+                int k = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    int n = int.Parse(Console.ReadLine());
+                    if (N > 0) k++;
+                }
+                System.Console.WriteLine(k);
+            }
+        }
+    }
+}
