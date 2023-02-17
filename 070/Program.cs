bed1c58e85b13c4a849b24da8d71aa9058f3665d
aleksandr sosuev <sav_ml@mail.ru>
2023-02-17 06:46:32 +0500

    Lesson seven - 070

---
+++ 070/Program.cs
@@ -0,0 +1,11 @@
+﻿// 70. Показать натуральные числа от 1 до N, N задано
+string NatChislo(int n)
+{
+    if (n == 1) return "1";
+    else return NatChislo(n - 1) + " " + n;
+}
+
+System.Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(NatChislo(n));
