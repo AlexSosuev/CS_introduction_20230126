9db4984978ea87e7bf589187d89f49e4c46a2fe3
aleksandr sosuev <sav_ml@mail.ru>
2023-02-17 06:55:20 +0500

    Lesson seven - 074

---
+++ 074/Program.cs
@@ -0,0 +1,11 @@
+﻿// 74. Найти сумму цифр числа
+int Sum(int n)
+{
+    if (n < 10) return n;
+    else return n % 10 + Sum(n / 10);
+}
+
+System.Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Сумма цифр числа {n} равна {Sum(n)}");
