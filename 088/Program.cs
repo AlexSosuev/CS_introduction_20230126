a7e953e9b05674a1e4ae379b8d2a19d0aab76975
Alexander Sosuev <sav_ml@mail.ru>
2023-02-21 09:50:12 +0500

    Lesson eight - 088

---
+++ 088/Program.cs
@@ -0,0 +1,9 @@
+﻿// 88. Заменить все вхождения подстроки w в строке st на строку v. Вывести на экран первоначальную строку и 
+//конечную строку.
+using System.Text;
+string st = Console.ReadLine();
+System.Console.WriteLine($"Исходная строка: {st}"); 
+StringBuilder st1 = new StringBuilder(st);
+for(int i=0;i<st1.Length;i++)
+  if (st1[i]=='w') st1[i]='v';
+System.Console.WriteLine($"Конечная строка: {st1}");   
